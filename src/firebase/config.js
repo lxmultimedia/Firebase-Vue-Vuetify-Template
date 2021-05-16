954df0c6128c06e1f90ecc3078b4103d8696900c
Attilio Furci <lxmediacom@gmail.com>
2021-05-16 17:35:11 +0200

    gitignore

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+};
+
+exports.firebaseConfig = firebaseConfig;
